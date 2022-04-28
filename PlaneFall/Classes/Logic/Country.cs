7e55af3187c8c3a34c6c4d0ffd90abad09f54012
mrproper06102003@gmail.com <mrproper06102003@gmail.com>
2022-04-28 23:05:29 +0300

    Начал писать класс Country

---
+++ PlaneFall/Classes/Logic/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneFall.Classes.Logic
+{
+    class Country
+    {
+        public static List<Country> Countries { get; private set; }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        static Country()
+        {
+
+        }
+        public Country() { }
+    }
+}
